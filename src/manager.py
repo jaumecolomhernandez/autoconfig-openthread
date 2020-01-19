1520f0fce7b4e88c0670e000d96e41428fb99675
Hector <rcpascualhector@gmail.com>
2020-01-19 20:16:21 +0100

    int cast

--- src/manager.py
+++ src/manager.py
@@ -198,7 +198,7 @@ class DeviceManager(object):
         if "VAUTH" in message[0]:
             m = message[0].split('_')
             dev = self.add_UDPDevice(self.internal_server.server_socket, (m[1],address_tuple[1])
-            dev.id = m[1]
+            dev.id = int(m[1])
             dev.connexion = True
             self.log.info(f"Added device to list ({m[1]},{address_tuple[1]})") 
 
